f91f64bda205f4d76dac83e4ae8cf4d0ba06d977
Owen Rumney <owen@owenrumney.co.uk>
2021-02-07 13:08:43 +0000

    fix the scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-BINARY=squealer
-CHECK_GEN_BINARY=squealer-checkgen
+
 TAG=${TRAVIS_TAG:-development}
 GO111MODULE=on
 export CGO_ENABLED=0
 export GOFLAGS=-mod=vendor
 args=(-ldflags "-X github.com/owenrumney/squealer/version.Version=${TAG} -s -w -extldflags '-fno-PIC -static'")
 
-GOOS=darwin GOARCH=amd64 go build -o bin/${BINARY}-darwin-amd64 "${args[@]}" ./cmd/squealer/
-GOOS=linux GOARCH=amd64 go build -o bin/${BINARY}-linux-amd64 "${args[@]}" ./cmd/squealer/
-GOOS=windows GOARCH=amd64 go build -o bin/${BINARY}-windows-amd64.exe "${args[@]}" ./cmd/squealer/
+GOOS=darwin GOARCH=amd64 go build -o bin/squealer.darwin.amd64 "${args[@]}" ./cmd/squealer/
+GOOS=linux GOARCH=amd64 go build -o bin/squealer.linux.amd64 "${args[@]}" ./cmd/squealer/
+GOOS=windows GOARCH=amd64 go build -o bin/squealer.windows.amd64.exe "${args[@]}" ./cmd/squealer/
