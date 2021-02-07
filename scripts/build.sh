0f8531d1d9242435066770a983266559ed318e6f
Owen Rumney <owen@owenrumney.co.uk>
2021-02-07 12:42:17 +0000

    ipdate the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,9 +7,6 @@ export CGO_ENABLED=0
 export GOFLAGS=-mod=vendor
 args=(-ldflags "-X github.com/owenrumney/squealer/version.Version=${TAG} -s -w -extldflags '-fno-PIC -static'")
 
-mkdir -p bin/darwin
-GOOS=darwin GOARCH=amd64 go build -o bin/darwin/${BINARY}-darwin-amd64 "${args[@]}" ./cmd/squealer/
-mkdir -p bin/linux
-GOOS=linux GOARCH=amd64 go build -o bin/linux/${BINARY}-linux-amd64 "${args[@]}" ./cmd/squealer/
-mkdir -p bin/windows
-GOOS=windows GOARCH=amd64 go build -o bin/windows/${BINARY}-windows-amd64.exe "${args[@]}" ./cmd/squealer/
+GOOS=darwin GOARCH=amd64 go build -o bin/${BINARY}-darwin-amd64 "${args[@]}" ./cmd/squealer/
+GOOS=linux GOARCH=amd64 go build -o bin/${BINARY}-linux-amd64 "${args[@]}" ./cmd/squealer/
+GOOS=windows GOARCH=amd64 go build -o bin/${BINARY}-windows-amd64.exe "${args[@]}" ./cmd/squealer/
