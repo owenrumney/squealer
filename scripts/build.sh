b99264a331804b3794fe4311f50d4c82a40e0220
Owen Rumney <owen@owenrumney.co.uk>
2021-02-07 13:22:28 +0000

    add windows build

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ export CGO_ENABLED=0
 export GOFLAGS=-mod=vendor
 args=(-ldflags "-X github.com/owenrumney/squealer/version.Version=${TAG} -s -w -extldflags '-fno-PIC -static'")
 
-GOOS=darwin GOARCH=amd64 go build -o bin/squealer.darwin.amd64 "${args[@]}" ./cmd/squealer/
-GOOS=linux GOARCH=amd64 go build -o bin/squealer.linux.amd64 "${args[@]}" ./cmd/squealer/
-GOOS=windows GOARCH=amd64 go build -o bin/squealer.windows.amd64.exe "${args[@]}" ./cmd/squealer/
+GOOS=darwin GOARCH=amd64 go build -o "bin/squealer.darwin.amd64.${TAG}" "${args[@]}" ./cmd/squealer/
+GOOS=linux GOARCH=amd64 go build -o "bin/squealer.linux.amd64.${TAG}" "${args[@]}" ./cmd/squealer/
+GOOS=windows GOARCH=amd64 go build -o "bin/squealer.windows.amd64.${TAG}.exe" "${args[@]}" ./cmd/squealer/
