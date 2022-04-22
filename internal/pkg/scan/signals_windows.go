fbfe513d7164e54b30abbde7a52263bf3bf1dca9
Owen Rumney <owen@owenrumney.co.uk>
2022-04-22 14:27:26 +0100

    fix: Fix the signal
    Signed-off-by: Owen Rumney <owen@owenrumney.co.uk>

--- internal/pkg/scan/signals_windows.go
+++ internal/pkg/scan/signals_windows.go
@@ -11,7 +11,7 @@ import (
 	"syscall"
 )
 
-func (s *gitScanner) monitorSignals(processes int, wg sync.WaitGroup) {
+func (s *gitScanner) monitorSignals(processes int, wg *sync.WaitGroup) {
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
